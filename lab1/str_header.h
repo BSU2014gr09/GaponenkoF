aedfad775aa1e8c03d78effd6a53d6bb6fbc2f41
fgaponenko <f.gaponenko7@gmail.com>
2015-03-17 13:23:22 +0300

    added lab1

---
+++ lab1/str_header.h
@@ -0,0 +1,10 @@
+#ifndef STR_HEADER
+#define STR_HEADER
+
+//void initial(char *str,int len);
+int maxS(int *arr,int size);
+char* allocate(int len);
+bool initial(char *str,int len);
+
+
+#endif // STR_HEADER
