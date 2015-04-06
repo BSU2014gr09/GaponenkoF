123ba672be1b889514c6f3999699c528ad684b42
fgaponenko <f.gaponenko7@gmail.com>
2015-04-06 14:51:08 +0300

    updated lab1

---
+++ lab1/str_header.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "str_header.h"
+using namespace std;
+char* allocate(int len)
+{
+	return new char[len];
+}
+bool initial(char *str,int len)
+{
+	cout<<"Enter str:"<<" "<<endl;
+	cin.getline(str,len);
+	cout<<str;
+	if(*str!=0)
+	return true;
+}
+int maxS(int *arr,int size)
+{
+    int sum=0;
+    int max=*arr;
+    int *p=arr;
+    while(size)
+    {
+        if(max<*p)
+            max=*p;
+        size--;
+        sum+=*p;
+        p++;
+    }
+     cout<<endl<<"sum"<<" "<<sum<<endl;
+    return max;
+}
--- lab1/str_header.h
+++ lab1/str_header.h
@@ -1,7 +1,6 @@
 #ifndef STR_HEADER
 #define STR_HEADER
 
-//void initial(char *str,int len);
 int maxS(int *arr,int size);
 char* allocate(int len);
 bool initial(char *str,int len);
