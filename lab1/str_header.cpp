ac06e160e195f07239e4b1aafcda4886879bbe85
fgaponenko <f.gaponenko7@gmail.com>
2015-04-06 19:08:40 +0300

    updated lab1

--- lab1/str_header.cpp
+++ lab1/str_header.cpp
@@ -29,3 +29,56 @@ int maxS(int *arr,int size)
      cout<<endl<<"sum"<<" "<<sum<<endl;
     return max;
 }
+int find_number(char *str)
+{
+    char *pstr=new char[strlen(str)];
+    strcpy(pstr,str);
+    char arr_s[10];
+    int arr_sum[10]= {0};
+    for (int i=0; i<10; i++)
+    {
+        *(arr_s+i)='0'+i;
+    }
+    while(*pstr)
+    {
+        if(*pstr>='0' && *pstr<='9')
+        {
+            for (int i=0; i<10; i++)
+            {
+                if(*(arr_s+i)==*pstr)
+                    arr_sum[i]++;
+            }
+        }
+        pstr++;
+    }
+    int sum=0;
+    for (int i=0; i<10; i++)
+    {
+        sum+=*(arr_sum+i)*i;
+    }
+    return sum;
+}
+
+
+void find_word(char *str)
+{
+    char *pStr=new char[strlen(str)];
+    strcpy(pStr,str);
+    int i=0,N=0;
+    char* m = strtok(pStr, " ");
+    while (m)
+    {
+        m = strtok(NULL, " ");
+        N++;
+    }
+    int *arr_sum = new int[N+1];
+    char* p = strtok(str, " ");
+    while (p)
+    {
+        *(arr_sum+i)=find_number(p);
+        p = strtok(NULL, " ");
+        i++;
+    }
+    int max = maxS(arr_sum,N+1);
+    cout<<"max:"<<max;
+}
--- lab1/str_header.h
+++ lab1/str_header.h
@@ -4,6 +4,7 @@
 int maxS(int *arr,int size);
 char* allocate(int len);
 bool initial(char *str,int len);
-
+int find_number(char *str);
+void find_word(char *str);
 
 #endif // STR_HEADER
