b1fbc452e58bec4e2b086199ec31dd81715653c4
fgaponenko <f.gaponenko7@gmail.com>
2015-06-04 17:20:48 +0300

    updated lab4

--- lab4/laba4.cpp
+++ lab4/laba4.cpp
@@ -25,18 +25,28 @@ void doing()
     cout<<str;
     char *str2=new char[2*strlen(str)];
     char *pstr = str;
+    int br=0;
 
     int j=0;
     char pr=3;//приоритет
     char ks;//переменная для удаление открывающих скобок
     bool flag=false;
     char qs='t';
+    bool fr =true;
+    bool brfl=true;
 
     str2[0]=0;
-    while(*pstr)
+    while(*pstr && (*pstr<'A' || *pstr>'Z' && *pstr<'a' || *pstr>'z') )
     {
         if((*pstr<'0' || *pstr>'9') && *pstr!='.')
         {
+            if(!fr)
+            {
+                cout<<endl<<"error"<<endl;
+                break;
+            };
+            if(*pstr!='(' && *pstr!=')')
+            fr=false;
             if(*pstr==' ' || *pstr==',')
             {
                 pstr++;
@@ -54,15 +64,35 @@ void doing()
                 };
                 while(opz!=0 && prior(*pstr)<=pr && prior(opz->arg)>0)//проверяем приоритет
                 {
-                        str2[j]=' ';
-                        str2[j+1]=pop(opz);//извлекаем элемент из стека
-                        j+=2;
+                    str2[j]=' ';
+                    str2[j+1]=pop(opz);//извлекаем элемент из стека
+                    j+=2;
                 };
+                if(*pstr=='(')
+                {
+                    br++;
+                    if(*(pstr-1)=='(')
+                    fr=true;
+                }
                 push(opz,*pstr);//отправляем элемент в стек
                 pr=prior(*pstr);
             }
             else
             {
+                if(*pstr==')')
+                  {
+                    br--;
+                    if(*(pstr-1))
+                    fr=true;
+                    }
+                if(pstr==str+strlen(str)-1)
+                fr=true;
+                if(br<0)
+                {
+                    cout<<endl<<"error";
+                    brfl=false;
+                    break;
+                }
                 while(opz && prior(opz->arg)>0)
                 {
                     str2[j]=' ';
@@ -76,24 +106,27 @@ void doing()
         }
         else
         {
+            fr=true;
             if(flag)
             {
                 str2[j]=' ';
                 j++;
                 flag=false;
-            }
+            };
             str2[j]=*pstr;
             j++;
         };
         pstr++;
     }
-
+    if(fr)
+    {
     while(opz)//извлекаем оставшиеся элементы из стека
     {
         str2[j]=pop(opz);
         j++;
     };
     cout<<endl<<str2;
+    }
 }
 
 int main()
